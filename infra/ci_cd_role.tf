fc6d3bd300f8263551763d5af07e71d762ef2e79
Kamalessh Velusamy <kamalessh.velusamy@gmail.com>
2025-10-27 08:04:00 -0400

    Add CI/CD OIDC role Terraform definition

---
+++ infra/ci_cd_role.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "github_oidc_role" {
+  name = "GitHubOIDCDeployRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Federated = "arn:aws:iam::<your-account-id>:oidc-provider/token.actions.githubusercontent.com" }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          "token.actions.githubusercontent.com:sub" : "repo:<your-github-user>/<your-repo>:ref:refs/heads/main"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_oidc_policy" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
